e1a3726a6abcd9aa1c7b627e3c12dba3b36d9d05
Yasir Mohamed <mohamey@tcd.ie>
2016-11-01 20:29:25 +0000

    Finished client and server code

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+server_address = ('localhost', 8000)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.settimeout(1)
+sock.connect((server_address))
+
+try:
+    echoMessage = "someMessage"
+    message = "GET /server.php?message={} HTTP/1.1\n\n".format(echoMessage).encode()
+    sock.send(message)
+
+    data = b''
+
+    while True:
+        print("Entered loop")
+        newData = sock.recv(4096)
+        if newData:
+            data += newData
+        else:
+            break
+
+    print(data.decode("utf-8"))
+finally:
+    print("Closing Socket")
+    sock.close()
